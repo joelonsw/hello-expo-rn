0ec68788a884ce9141df63b0739eed2d8211c6e9
joelonsw <joel610naver@gmail.com>
2024-03-03 14:19:09 +0900

    feat: tutorial done

--- App.js
+++ App.js
@@ -1,23 +1,34 @@
+import { useState, useRef } from 'react';
 import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, View} from 'react-native';
+import { ImageBackground, StyleSheet, View, Platform } from 'react-native';
 import ImageViewer from './components/ImageViewer';
 import Button from "./components/Button";
 import * as ImagePicker from 'expo-image-picker';
-import { useState } from 'react';
 import IconButton from "./components/IconButton";
 import CircleButton from "./components/CircleButton";
 import EmojiPicker from "./components/EmojiPicker";
 import EmojiList from './components/EmojiList';
 import EmojiSticker from './components/EmojiSticker';
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import * as MediaLibrary from 'expo-media-library';
+import { captureRef } from 'react-native-view-shot';
+import domtoimage from 'dom-to-image';
 
 const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
+    const image = { uri: "https://docs.expo.dev/static/images/tutorial/splash.png" };
+
+    const imageRef = useRef();
     const [isModalVisible, setIsModalVisible] = useState(false);
     const [showAppOptions, setShowAppOptions] = useState(false);
     const [selectedImage, setSelectedImage] = useState(null);
     const [pickedEmoji, setPickedEmoji] = useState(null);
+
+    const [status, requestPermission] = MediaLibrary.usePermissions();
+    if (status === null) {
+        requestPermission();
+    }
 
     const onReset = () => {
         setShowAppOptions(false);
@@ -31,7 +42,36 @@ export default function App() {
     };
 
     const onSaveImageAsync = async () => {
-        //
+        if (Platform.OS !== 'web') {
+            try {
+                const localUri = await captureRef(imageRef, {
+                    height: 440,
+                    quality: 1,
+                });
+
+                await MediaLibrary.saveToLibraryAsync(localUri);
+                if (localUri) {
+                    alert("Saved");
+                }
+            } catch (e) {
+                console.log(e);
+            }
+        } else {
+            try {
+                const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+                    quality: 0.95,
+                    width: 320,
+                    height: 440
+                });
+
+                let link = document.createElement('a');
+                link.download = 'sticker-smash.jpeg';
+                link.href = dataUrl;
+                link.click();
+            } catch (e) {
+                console.log(e);
+            }
+        }
     };
 
     const pickImageAsync = async () => {
@@ -50,12 +90,15 @@ export default function App() {
 
     return (
         <GestureHandlerRootView style={styles.container}>
+            <ImageBackground source={image} style={styles.image} />
             <View style={styles.imageContainer}>
-                <ImageViewer
-                    placeholderImageSource={PlaceholderImage}
-                    selectedImage={selectedImage}
-                />
-                {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />}
+                <View ref={imageRef} collapsable={false}>
+                    <ImageViewer
+                        placeholderImageSource={PlaceholderImage}
+                        selectedImage={selectedImage}
+                    />
+                    {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />}
+                </View>
             </View>
             {showAppOptions ? (
                 <View style={styles.optionsContainer}>
@@ -74,7 +117,7 @@ export default function App() {
             <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
                 <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
             </EmojiPicker>
-            <StatusBar style="auto"/>
+            <StatusBar style="light"/>
         </GestureHandlerRootView>
     );
 }
